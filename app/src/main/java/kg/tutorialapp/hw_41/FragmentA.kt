79603fa5aa29999efe11c96c3e67bacfd0738a4e
Kunduz <talaybekovna.kunduz@gmail.com>
2021-05-26 20:55:16 +0600

    add three fragments

---
+++ app/src/main/java/kg/tutorialapp/hw_41/FragmentA.kt
@@ -0,0 +1,21 @@
+package kg.tutorialapp.hw_41
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class FragmentA : Fragment(R.layout.fragment_a) {
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    companion object {
+
+    }
+}
